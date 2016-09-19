02598ee99adc0e6cd16fc14140bf1c8e9c247199
Shmuel Kallner <kallner@il.ibm.com>
2016-09-19 14:28:20 +0300

    Add API documentation (#7)
    * IBM-Swift/Kitura#726 Added API documentation and HTML generation
    
    * IBM-Swift/Kitura#726 Updated Copyright
    
    * IBM-Swift/Kitura#726 Removed toolchain, no longer needed as we are on Swift 3.0
    
    * IBM-Swift/Kitura#726 Fixed typo
    
    * IBM-Swift/Kitura#726 Fixed typo
    
    * IBM-Swift/Kitura#726 Added generated documentation

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Kitura-TemplateEngine
-Kitura Template Engine protocol 
+The Kitura Template Engine protocol. Implemented by Templating Engines in order to
+integrate with Kitura's content generation APIs.
 
 Inspired by http://expressjs.com/en/guide/using-template-engines.html.
--- Sources/KituraTemplateEngine/TemplateEngine.swift
+++ Sources/KituraTemplateEngine/TemplateEngine.swift
@@ -1,5 +1,5 @@
-/**
- * Copyright IBM Corporation 2015
+/*
+ * Copyright IBM Corporation 2015 - 2016
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,12 +12,25 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- **/
+ */
 
-// Template Engine for Kitura
-// influenced by http://expressjs.com/en/guide/using-template-engines.html
 
+/// Template Engine protocol for Kitura. Implemented by Templating Engines in order to
+/// integrate with Kitura's content generation APIs.
+///
+/// - Note: Influenced by http://expressjs.com/en/guide/using-template-engines.html
 public protocol TemplateEngine {
+    
+    /// The file extension of files in the views directory that will be
+    /// rendered by a particular templating engine.
     var fileExtension: String { get }
+    
+    /// Take a template file and a set of "variables" in the form of a context
+    /// and generate content to be sent back to the client.
+    ///
+    /// - Parameter filePath: The path of the template file to use when generating
+    ///                      the content.
+    /// - Parameter context: A set of variables in the form of a Dictionary of
+    ///                     Key/Value pairs, that can be used when generating the content.
     func render(filePath: String, context: [String: Any]) throws -> String
 }
