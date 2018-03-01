d432d61cd9dfb66bc9741fec44434bbbcad676cd
David Jones <djones6dev@gmail.com>
2018-03-01 10:46:45 +0000

    Restore swift 3.1 support (#23)

--- Package.swift
+++ Package.swift
@@ -1,6 +1,3 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 /**
  * Copyright IBM Corporation 2016, 2017
  *
@@ -20,19 +17,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "Kitura-TemplateEngine",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "KituraTemplateEngine",
-            targets: ["KituraTemplateEngine"]
-        )
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "KituraTemplateEngine"
-        )
-    ]
+    name: "Kitura-TemplateEngine"
 )
